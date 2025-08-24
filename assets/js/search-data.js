ea2ff4fd6c415f230fac331f00a8a683fa5966be
vedantphad <123496456+vedantphad@users.noreply.github.com>
2025-08-24 05:03:34 +0000

    Deploying to gh-pages from @ vedantphad/vedantphad.github.io@80643b1f8f6e08d8fb222dfdc96fc4286b122d1b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-gallery",
-          title: "Gallery",
-          description: "Some moments of college life",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/gallery/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-gallery",
+          title: "Gallery",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/gallery/";
           },
         },{id: "post-a-post-with-plotly-js",
         
